a421579d036500a11ed5ec9c2deffd959725fc6b
Razvan Certezeanu <razvan.certezeanu14@imperial.ac.uk>
2016-11-02 14:07:04 +0000

    [R.C][C.A] Given the latitude and longitude, sends http request to get the location using Google Places API. Marks and tags the location on map.

--- app/src/main/java/tripster/tripster/PhotosOnMapFragment.java
+++ app/src/main/java/tripster/tripster/PhotosOnMapFragment.java
@@ -3,6 +3,14 @@ package tripster.tripster;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,6 +19,12 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 
 public class PhotosOnMapFragment extends Fragment implements OnMapReadyCallback {
 
@@ -21,6 +35,7 @@ public class PhotosOnMapFragment extends Fragment implements OnMapReadyCallback 
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getActivity().setContentView(R.layout.activity_maps);
+        parseLocationFile(null);
         SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -28,10 +43,10 @@ public class PhotosOnMapFragment extends Fragment implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        //TODO: Remove this. Added for guidance and testing.
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney  = new LatLng(-34, 151);
-        LatLng london  = new LatLng(47, 0.132);
+        LatLng sydney = new LatLng(-34, 151);
+        LatLng london = new LatLng(47, 0.132);
         LatLng london1 = new LatLng(49, -0.132);
         LatLng london2 = new LatLng(48.3, 0.532);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -44,5 +59,37 @@ public class PhotosOnMapFragment extends Fragment implements OnMapReadyCallback 
                 icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
         // newMarker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.picture));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void parseLocationFile(File file) {
+        final LatLng newLocation = new LatLng(51.507653, -0.165726);
+        String url2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
+             "location=51.507653,-0.165726&radius=100&key=AIzaSyBEcADKicF0ZeIooOSbh12Vu7BVyDOIjik";
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url2,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Log.d("RESPONSE request place", response);
+                            JSONObject clientObject = new JSONObject(response);
+                            JSONArray array = clientObject.getJSONArray("results");
+                            String placeName = array.getJSONObject(1).getString("name");
+                            mMap.addMarker(new MarkerOptions().position(newLocation).title(placeName));
+                            Log.d("Place Name",placeName);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Utils", "Didn't work request queue");
+                    }
+                });
+
+        queue.add(stringRequest);
     }
 }
