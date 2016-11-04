854e1a6acb34c0ccf11cbe04209b5e2838a45e86
agk14 <andreea.kevorchian14@imperial.ac.uk>
2016-11-04 12:03:12 +0000

    Add gripd view to home screen.

---
+++ app/src/main/java/tripster/tripster/TripPreviewAdapter.java
@@ -0,0 +1,51 @@
+package tripster.tripster;
+
+import android.app.Activity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class TripPreviewAdapter extends BaseAdapter {
+  private Activity activity;
+  private String[] tripNames = {"Ongoing Trip", "Trip", "Trip", "Trip"};
+
+  public TripPreviewAdapter(Activity activity) {
+    this.activity = activity;
+  }
+
+  public int getCount() {
+    return thumbIds.length;
+  }
+
+  public Object getItem(int position) {
+    return null;
+  }
+
+  public long getItemId(int position) {
+    return 0;
+  }
+
+  public View getView(int position, View convertView, ViewGroup parent) {
+    View rowView;
+    if (convertView == null) {  // if it's not recycled, initialize some attributes
+      LayoutInflater inflater = activity.getLayoutInflater();
+      rowView = inflater.inflate(R.layout.photos_list, null,true);
+      TextView txtTitle = (TextView) rowView.findViewById(R.id.photo_info);
+      ImageView imageView = (ImageView) rowView.findViewById(R.id.photo);
+
+      txtTitle.setText(tripNames[position]);
+      txtTitle.setGravity(Gravity.CENTER);
+      imageView.setImageResource(thumbIds[position]);
+    } else {
+      rowView = convertView;
+    }
+    return rowView;
+  }
+
+  private Integer[] thumbIds = {
+      R.drawable.bubble_mask, R.drawable.bubble_mask, R.drawable.bubble_mask, R.drawable.bubble_mask};
+}
---
+++ app/src/main/java/tripster/tripster/fragments/MyTripsFragment.java
@@ -0,0 +1,35 @@
+package tripster.tripster.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import tripster.tripster.R;
+import tripster.tripster.TripPreviewAdapter;
+
+public class MyTripsFragment extends Fragment {
+  private static final String TAG = MyTripsFragment.class.getName();
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_trips, container, false);
+    GridView grid = (GridView) view.findViewById(R.id.myTrips);
+    grid.setAdapter(new TripPreviewAdapter(getActivity()));
+    grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d(TAG, "Here I am");
+        HomeFragment frag = new HomeFragment();
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_content, frag).commit();
+      }
+    });
+    return view;
+  }
+}
