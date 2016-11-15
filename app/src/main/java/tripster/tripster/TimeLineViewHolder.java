1dceec03782d347d327a3fc41ebdca53380cc4e1
Razvan Certezeanu <razvan.certezeanu14@imperial.ac.uk>
2016-11-15 21:12:50 +0000

    [R.C][C.A] Adds horizontal view for photos in timeline.

--- app/src/main/java/tripster/tripster/TimeLineViewHolder.java
+++ app/src/main/java/tripster/tripster/TimeLineViewHolder.java
@@ -1,20 +1,53 @@
 package tripster.tripster;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by HP-HP on 05-12-2015.
  */
 public class TimeLineViewHolder extends RecyclerView.ViewHolder {
     public TextView name;
-    public  TimelineView mTimelineView;
+    public TimelineView mTimelineView;
+    private Bitmap imagebm;
 
     public TimeLineViewHolder(View itemView, int viewType) {
         super(itemView);
         name = (TextView) itemView.findViewById(R.id.tx_name);
         mTimelineView = (TimelineView) itemView.findViewById(R.id.time_marker);
         mTimelineView.initLine(viewType);
+        AsyncTask asyncTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                try {
+                    imagebm = BitmapFactory
+                            .decodeStream((InputStream) new URL("https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E.jpg").getContent());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+        asyncTask.execute();
+
+        LinearLayout layout = (LinearLayout) itemView.findViewById(R.id.linear);
+        ImageView imageView = new ImageView(itemView.getContext());
+        imageView.setImageBitmap(imagebm);
+        imageView.setPadding(2, 2, 2, 2);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imageView.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(imageView);
     }
 }
